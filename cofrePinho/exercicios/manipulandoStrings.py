6f79bcc9a5c97eab461e524151f98a7d2bc9b0f7
Emerson pinho <the.emerson.araujo@gmail.com>
2025-01-17 08:47:44 -0300

    arq de exercicios em py

---
+++ cofrePinho/exercicios/manipulandoStrings.py
@@ -0,0 +1,20 @@
+"Copa 2014"
+
+'Copa do mundo 2014'
+
+'''2014 - Copa do mundo
+'''
+
+"copa 'padrão fifa'"
+
+'copa "padrão fifa"'
+
+print("""\
+uso: consulta_base [0PC)ES]
+     -h               exibe saída de ajuda
+     -u url           url do dataser""")
+
+("copa" "2014") == "copa2014"
+
+input('Em qual cidade o legado da copa foi mais relevante '
+      'para a população?')
