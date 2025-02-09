010295e5060d832f626235378b0377c2fe30c27a
Emerson pinho <the.emerson.araujo@gmail.com>
2025-02-09 10:06:42 -0300

    Add files via upload

---
+++ cofrePinho/exercicios/problemasNoFor.py
@@ -0,0 +1,5 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for item in lista:
+    print(item, end=", ")
+
+# Mas e se quiséssemos imprimir os números de 0 até 100? Certamente não temos que criar manualmente uma lista com esses elementos. Vamos ver mais elementos que combinam com o for.
